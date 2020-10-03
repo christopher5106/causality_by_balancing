804473ed27b624c1250443d326ff7cd7444dd628
Christopher Bourez <cbo@ivalua.com>
2020-10-03 18:42:26 +0200

    n

--- train.py
+++ train.py
@@ -55,15 +55,15 @@ for epoch in range(n_epochs):
             p_a.grad.zero_()
             # p_b_a.grad.zero_()
 
-        # # print statistics
-        # running_loss += loss.item()
-        # if i % 2000 == 1999:    # print every 2000 mini-batches
-        #     print('[%d, %5d] loss: %.3f' %
-        #           (epoch + 1, i + 1, running_loss / 2000))
-        #     running_loss = 0.0
-    print(f"True p_a {true_p_a} {torch.nn.functional.softmax(p_a, dim=0)}")
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' %
+                  (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
     # print(f"True p_b_a {true_p_b_a} {p_b_a}")
 
 print('Finished Training')
-print(f"True p_a {true_p_a} {p_a}")
-print(f"True p_b_a {true_p_b_a} {p_b_a}")
+print(f"True p_a {true_p_a} {torch.nn.functional.softmax(p_a, dim=0)}")
+# print(f"True p_b_a {true_p_b_a} {p_b_a}")
